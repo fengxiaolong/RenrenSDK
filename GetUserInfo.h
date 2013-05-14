3911b342af6f4b02cd6c3411a4343ef6d9d9aea6
fengxiaolong <aldrichnjit@gmail.com>
2013-05-14 19:35:17 +0800

    delete comments

--- GetUserInfo.h
+++ GetUserInfo.h
@@ -5,7 +5,6 @@
 #include <curl/curl.h>
 #define URL "https://api.renren.com/restserver.do"
 #define USERINFOURL "v=1.0&access_token="
-//234283%7C6.d33d4719c2359c91abe4c7d19a9339b5.2592000.1371092400-252633228&format=JSON&call_id=1.0&method=users.getInfo&fields=name,sex,uid"
 int GetUserInfo(char *access_token);
 static size_t write_data(char *ptr,size_t size,size_t nmemb,void *userdata);
 #endif
