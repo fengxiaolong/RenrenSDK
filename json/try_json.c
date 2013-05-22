67f90218309efe27a78b6d16845b808dab8c3bf6
fengxiaolong <aldrichnjit@gmail.com>
2013-05-22 22:00:55 +0800

    add

--- json/try_json.c
+++ json/try_json.c
@@ -10,7 +10,7 @@ int main(int arg,char **argv)
     {
         JsonParser *parser;
         GError *error;
-        g_type_init();
+        //        g_type_init();
         parser = json_parser_new();
         json_parser_load_from_file(parser,"userinfo.json",&error);
         if(error)
@@ -28,22 +28,27 @@ int main(int arg,char **argv)
         JsonObject *jsonobject;
         jsonobject = json_object_new();
         gboolean hasmember;
+        jsonobject = json_node_get_object(root);
+        guint cc;
+        cc = json_object_get_size(jsonobject);
+        g_printf("this object size is %d\n",cc);
         if(JSON_NODE_OBJECT == nodetype)
             {
                 puts("object\n");
                 hasmember = json_object_has_member(jsonobject,"name");
                 if(hasmember)
+                {
                     puts("has member\n");
+                    JsonNode *newnode = json_object_get_member(jsonobject,"name");
+                    g_print("the internal data type is %d\n",JSON_NODE_TYPE(newnode));
+                    g_print("name is :%s\n",json_node_get_string(newnode));
+                }
                 else
                     puts("has no member\n");
-                
             }
         if(JSON_NODE_ARRAY == nodetype)
             {
                 puts("array\n");
             }
-        
-        
-        
         return 0;
     }
