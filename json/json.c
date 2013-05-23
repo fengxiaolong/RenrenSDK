90a2ea468e30617f7cf1101c3e8f61e96489bc9d
fengxiaolong <aldrichnjit@gmail.com>
2013-05-23 18:12:34 +0800

    fix segment bug,error pointer better initialize to NULL

--- json/json.c
+++ json/json.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
+#include <error.h>
 #include <glib-object.h>
 #include <json-glib/json-glib.h>
+#include <json-glib/json-gobject.h>
 int main(int argc,char **argv)
     {
         JsonParser *parser;
         GError *error;
+        g_type_init();
+        error = NULL;
         parser = json_parser_new();
         json_parser_load_from_file(parser,"info.json",&error);
         if(error)
@@ -17,25 +21,27 @@ int main(int argc,char **argv)
                 return EXIT_FAILURE;
             }
         JsonNode *root;
-        //get the first node of json stream
         root = json_parser_get_root(parser);
-        //judge the type of root
         if(JSON_NODE_HOLDS_ARRAY(root))
             {
                 puts("hold array\n");
                 JsonArray *array;
-                //array = json_array_new();
-                //get the array stored in the node
+                array = json_array_new();
                 array = json_node_get_array(root);
                 g_print("the length of the array is %d\n",json_array_get_length(array));
                 root = json_array_get_element(array,0);
                 if(JSON_NODE_HOLDS_OBJECT(root))
-                    puts("hold object\n");
-                if(JSON_NODE_HOLDS_ARRAY(root))
-                    puts("hold array\n");
-                
+                    {
+                        JsonObject *newobj;
+                        newobj = json_node_get_object(root);
+                        guint a;
+                        a = json_object_get_size(newobj);
+                        printf("the size of this object is %d\n",a);
+                        const gchar *str;
+                        str = json_object_get_string_member(newobj,"birthday");
+                        g_print("birthday is %s \n",str);
+                    }
+       
             }
-        else
-            g_print("error\n");
         return EXIT_SUCCESS;
     }
